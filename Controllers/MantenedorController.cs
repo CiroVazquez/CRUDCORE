a24eb706f96e59fe6fc0b03a0476efdfcdfc2458
Ciro Vazquez <112038455+CiroVazquez@users.noreply.github.com>
2023-03-31 19:25:27 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/MantenedorController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using CRUDCORE.Datos;
+using CRUDCORE.Models;
+
+
+namespace CRUDCORE.Controllers
+{
+    public class MantenedorController : Controller
+    {
+
+
+
+        ContactoDatos _ContactoDatos = new ContactoDatos();
+
+
+
+        public IActionResult listar() //mostrara una lista de contactos
+        {
+            var oLista = _ContactoDatos.Listar();
+            return View(oLista);
+        }
+
+
+
+
+        public IActionResult guardar() //devolver vista, formulario html
+        {
+            return View();
+        }
+
+
+
+
+
+        [HttpPost]
+        public IActionResult guardar(ContactoModel oContacto) //recibir un objeto y guardarlo en base de datos
+        {
+            
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var respuesta = _ContactoDatos.guardar(oContacto);
+            if (respuesta)
+            {
+                return RedirectToAction("listar");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+
+
+        public IActionResult editar(int idContacto) //devolver vista, formulario html
+        {
+            var oContacto = _ContactoDatos.Obtener(idContacto);
+            return View(oContacto);
+        }
+
+
+
+
+        [HttpPost]
+        public IActionResult editar(ContactoModel oContacto) 
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var respuesta = _ContactoDatos.editar(oContacto);
+            if (respuesta)
+            {
+                return RedirectToAction("listar");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+
+
+        public IActionResult eliminar(int idContacto) //devolver vista, formulario html
+        {
+            var oContacto = _ContactoDatos.Obtener(idContacto);
+            return View(oContacto);
+        }
+
+
+
+
+        [HttpPost]
+        public IActionResult eliminar(ContactoModel oContacto)
+        {
+            
+            var respuesta = _ContactoDatos.eliminar(oContacto.idContacto);
+            if (respuesta)
+            {
+                return RedirectToAction("listar");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Datos/ContactoDatos.cs
@@ -0,0 +1,168 @@
+﻿using CRUDCORE.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CRUDCORE.Datos
+{
+    public class ContactoDatos
+    {
+
+        public List<ContactoModel> Listar()
+        {
+            var oLista = new List<ContactoModel>();
+
+            var cn = new Conexion();
+
+            using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("sp_Listar", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        oLista.Add(new ContactoModel()
+                        {
+                            idContacto = Convert.ToInt32(dr["idContacto"]),
+                            nombre = dr["nombreContacto"].ToString(),
+                            telefono = dr["telefonoContacto"].ToString(),
+                            correo = dr["correoContacto"].ToString(),
+                        });
+                    }
+                }
+            }
+            return oLista;
+        }
+
+
+
+
+
+
+        public ContactoModel Obtener(int IdContacto)
+        {
+            var oContacto = new ContactoModel();
+
+            var cn = new Conexion();
+
+            using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+            {
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand("sp_Obtener", conexion);
+                cmd.Parameters.AddWithValue("IdContacto", IdContacto);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        oContacto.idContacto = Convert.ToInt32(dr["idContacto"]);
+                        oContacto.nombre = dr["nombreContacto"].ToString();
+                        oContacto.telefono = dr["telefonoContacto"].ToString();
+                        oContacto.correo = dr["correoContacto"].ToString();
+                    }
+                }
+            }
+            return oContacto;
+        }
+
+
+
+        public bool guardar(ContactoModel oContacto)
+        {
+            bool rpta;
+
+            try
+            {
+                var cn = new Conexion();
+                using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+                {
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand("sp_Guardar", conexion);
+                    cmd.Parameters.AddWithValue("Nombre", oContacto.nombre);
+                    cmd.Parameters.AddWithValue("Telefono", oContacto.telefono);
+                    cmd.Parameters.AddWithValue("Correo", oContacto.correo);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+            }
+            catch ( Exception e)
+            {
+                string error = e.Message;
+                rpta = false;
+            }
+
+            return rpta;
+        }
+
+
+
+
+
+        public bool editar(ContactoModel oContacto)
+        {
+            bool rpta;
+
+            try
+            {
+                var cn = new Conexion();
+                using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+                {
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand("sp_Editar", conexion);
+                    cmd.Parameters.AddWithValue("IdContacto", oContacto.idContacto);
+                    cmd.Parameters.AddWithValue("Nombre", oContacto.nombre);
+                    cmd.Parameters.AddWithValue("Telefono", oContacto.telefono);
+                    cmd.Parameters.AddWithValue("Correo", oContacto.correo);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                rpta = false;
+            }
+
+            return rpta;
+        }
+
+
+
+
+
+
+        public bool eliminar(int idContacto)
+        {
+            bool rpta;
+
+            try
+            {
+                var cn = new Conexion();
+                using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+                {
+                    conexion.Open();
+                    SqlCommand cmd = new SqlCommand("sp_Eliminar", conexion);
+                    cmd.Parameters.AddWithValue("idContacto", idContacto);
+                    
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+                rpta = true;
+            }
+            catch (Exception e)
+            {
+                string error = e.Message;
+                rpta = false;
+            }
+
+            return rpta;
+        }
+
+
+    }
+}
